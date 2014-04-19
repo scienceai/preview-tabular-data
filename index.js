828d6282b8e3f6b5d590576a8c2e634bf182c212
sebastien ballesteros <sebastien.ballesteros@gmail.com>
2014-04-19 14:58:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,146 @@
+var path = require('path')
+  , binaryCSV = require('binary-csv')
+  , util = require('util')
+  , Trunc = require('truncating-stream')
+  , stream = require('stream')
+  , xlsx = require('xlsx')
+  , xls = require('xlsjs')
+  , once = require('once')
+  , CsvTsvPreview = require('./lib/csv-tsv-preview')
+  , split = require('split')
+  , concat = require('concat-stream');
+
+
+function previewCsvTsv(readable, contentType, contentLength, opts, callback){
+
+  if(arguments.length === 4){
+    callback = opts;
+    opts = {};
+  }
+
+  callback = once(callback);
+
+  var nPreview = opts.nPreview || 10;
+
+  var parser = binaryCSV({separator: (contentType === 'text/csv') ? ',': '\t'});
+  var p = new CsvTsvPreview(parser, {nPreview: nPreview});
+
+  var s =  readable.pipe(parser).pipe(p);
+  s.on('error', callback);
+  s.on('finish', function(){
+    readable.destroy();
+    callback(null, s.preview);
+  });
+
+};
+
+function previewXls(readable, contentType, contentLength, opts, callback){
+
+  if(arguments.length === 4){
+    callback = opts;
+    opts = {};
+  }
+
+  callback = once(callback);
+
+  var nPreview = opts.nPreview || 10;
+
+  readable.pipe(concat(function(data){
+
+    var parser = (contentType === 'application/vnd.ms-excel') ? xls: xlsx;
+
+    var workbook = parser.read(data, {type: 'binary'});
+
+    if(workbook.SheetNames.length>1){
+      console.error('multiple sheets in a workbook');
+    }
+
+    var sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    try {
+      var ldjson = parser.utils.sheet_to_row_object_array(sheet);
+    } catch(e){
+      return callback(e);
+    }
+
+    var preview = [ Object.keys(ldjson[0]) ];
+    for(var i=0, l = Math.min(ldjson.length, nPreview); i<(l-1); i++){
+      var arr = [] //push only hasOwnproperty
+      var row = ldjson[i];
+
+      preview[0].forEach(function(key){
+        arr.push(row[key]);
+      });
+      preview.push(arr);
+    }
+
+
+    callback(null, preview);
+
+  }));
+
+};
+
+function previewLdJson(readable, contentType, contentLength, opts, callback){
+
+  if(arguments.length === 4){
+    callback = opts;
+    opts = {};
+  }
+
+  callback = once(callback);
+
+  var maxSize = opts.maxSize || 1024000;
+  var nPreview = opts.nPreview || 10;
+
+  var s;
+
+  if(contentLength > maxSize){
+    var t = new Trunc({ limit : maxSize });
+    s = readable.pipe(t);
+    t.on('finish', function(){
+      readable.destroy();
+    });
+  } else {
+    s = readable;
+  }
+
+  var preview = [];
+
+  s = s.pipe(split(function(row){
+    if(row) {
+      return JSON.parse(row);
+    }
+  }));
+
+  s.on('data', function(data){
+    if(preview.length < nPreview){
+      preview.push(data);
+    }
+  });
+  s.on('error', callback);
+  s.on('end', function(){
+    callback(null, preview);
+  });
+
+};
+
+
+function preview(readable, contentType, contentLength, opts, callback){
+
+  if(contentType === 'application/x-ldjson'){
+    previewLdJson(readable, contentType, contentLength, opts, callback);
+  } else if (contentType === 'application/vnd.ms-excel' || contentType === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'){
+    previewXls(readable, contentType, contentLength, opts, callback);
+  } else if(contentType === 'text/csv' || contentType === 'text/tab-separated-values' ){
+    previewCsvTsv(readable, contentType, contentLength, opts, callback);
+  } else {
+    callback(new Error('no preview available for ' + contentType));
+  }
+
+};
+
+exports.preview = preview;
+exports.csvTsv = previewCsvTsv;
+exports.ldjson = previewLdJson;
+exports.xls = previewXls;
---
+++ test/test.js
@@ -0,0 +1,38 @@
+var util = require('util')
+  , fs = require('fs')
+  , path = require('path')
+  , assert = require('assert')
+  , preview = require('..').preview;
+
+var root = path.dirname(__filename);
+
+describe('preview', function(){
+
+  it('should preview csv', function(done){
+    fs.stat(path.join(root, 'fixtures', 'data.csv'), function(err, stats){
+      preview(fs.createReadStream(path.join(root, 'fixtures', 'data.csv')), 'text/csv', stats.size, {nPreview: 2}, function(err, preview){
+        assert.deepEqual(preview, [["a","b","c"], [1,2,3]]);
+        done();
+      });
+    });
+  });
+
+  it('should preview xlsx', function(done){
+    fs.stat(path.join(root, 'fixtures', 'data.xlsx'), function(err, stats){
+      preview(fs.createReadStream(path.join(root, 'fixtures', 'data.xlsx')), 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', stats.size, {nPreview: 2}, function(err, preview){
+        assert.deepEqual(preview, [["Non-wrinkled","Wrinkled"],["1.00","1.25"]] );
+        done();
+      });
+    });
+  });
+
+  it('should preview ldjson', function(done){
+    fs.stat(path.join(root, 'fixtures', 'data.ldjson'), function(err, stats){
+      preview(fs.createReadStream(path.join(root, 'fixtures', 'data.ldjson')), 'application/x-ldjson', stats.size, {nPreview: 2}, function(err, preview){
+        assert.deepEqual(preview, [{"a":1,"b":3,"c":41}, {"a":2,"b":3,"c":42}]);
+        done();
+      });
+    });
+  });
+
+});
